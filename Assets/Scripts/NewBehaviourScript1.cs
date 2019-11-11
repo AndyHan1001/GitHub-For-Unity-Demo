f35a40ad3192cc4444396558b8dafbb0c71bcf90
Dan Han <andyhan1001@hotmail.com>
2019-11-11 19:01:29 +0800

    second update
    create some scripts

---
+++ Assets/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour 
+{
+
+	// Use this for initialization
+	private void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+		
+	}
+}
