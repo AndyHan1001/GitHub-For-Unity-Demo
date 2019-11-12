efc9b737c3c5ee65063d7fd41288385220bbeb82
Dan Han <andyhan1001@hotmail.com>
2019-11-12 11:47:54 +0800

    111
    222

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	private void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+		
+	}
+}
