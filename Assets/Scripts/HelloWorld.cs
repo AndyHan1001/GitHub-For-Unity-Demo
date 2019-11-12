79113ee0cc497044a21f84df8a8e1bb3044d1f43
Dan Han <andyhan1001@hotmail.com>
2019-11-12 12:08:03 +0800

    implement "HelloWorld" script.
    Implementation: just print a string to the console.

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -2,11 +2,13 @@
 
 public class HelloWorld : MonoBehaviour 
 {
+    [SerializeField]
+    private string name;
 
 	// Use this for initialization
 	private void Start () 
 	{
-		
+		print(name);  // Just print the input string to the console.
 	}
 	
 	// Update is called once per frame
