4a9f7fcba00ddb76ea25780fbfe031df65b14625
Dan Han <andyhan1001@hotmail.com>
2019-11-12 12:00:55 +0800

    implement HelloWorld script
    simple implementation: just print a string to the console.

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -2,11 +2,12 @@
 
 public class HelloWorld : MonoBehaviour 
 {
+	private readonly string name = "Hello";
 
 	// Use this for initialization
 	private void Start () 
 	{
-		
+		print(name);
 	}
 	
 	// Update is called once per frame
